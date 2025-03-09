1abcb54fa316e26fc93efda0d5756791c50a3e69
Artur Fayzullin <fayzullin.ar@mail.ru>
2025-03-09 21:39:29 +0300

    add

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -3,6 +3,7 @@ package hexlet.code.schemas;
 public abstract class BaseSchema<T extends BaseSchema<T>> {
     protected boolean isRequired = false;
 
+    @SuppressWarnings("unchecked")
     public T required() {
         this.isRequired = true;
         return (T) this;
